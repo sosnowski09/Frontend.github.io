e49b751d92caacc516391697b8e86b94b1a220fe
Alejo Sosnowski <alejodamian09@gmail.com>
2023-02-28 03:19:08 -0300

    Css portafolio terminado. Footer terminado HTML/CSS. Menu responsive en proceso.

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
